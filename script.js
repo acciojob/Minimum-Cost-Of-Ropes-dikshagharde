d539d01767f35b61122372691d62b14039d7e35b
dikshagharde <117832798+dikshagharde@users.noreply.github.com>
2023-05-03 22:40:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,46 +1,22 @@
 function calculateMinCost() {
-const str = document.querySelector('#rope-lengths').value;
-const strArr = str.split(',');
-const arr = strArr.map(e => Number.parseInt(e)).filter(e => !isNaN(e));
+const ropeLengthsStr = document.getElementById('rope-lengths').value;
+const ropeLengthsArr = ropeLengthsStr.split(',').map(Number);
+const n = ropeLengthsArr.length;
 
-if (arr.length < 2) {
-document.querySelector('#result').innerText = "Error: Please enter at least two valid rope lengths.";
-return;
-}
-
-let n = arr.length;
-let ans = helper(arr, n);
-document.querySelector('#result').innerText = ans;
-}
-
-function helper(arr, n) {
-// Create a priority queue
 let pq = [];
-
-// Adding items to the pQueue
 for (let i = 0; i < n; i++) {
-pq.push(arr[i]);
+pq.push(ropeLengthsArr[i]);
 }
+pq.sort(function (a, b) { return a - b; });
 
-pq.sort(function(a, b) {
-return a - b;
-});
-
-// Initialize result
 let res = 0;
-
-// While size of priority queue
-// is more than 1
 while (pq.length > 1) {
-// Extract shortest two ropes from pq
 let first = pq.shift();
 let second = pq.shift();
-
-// Connect the ropes: update result
-// and insert the new rope to pq
 res += first + second;
 pq.push(first + second);
+pq.sort(function (a, b) { return a - b; });
 }
 
-return res;
-} 
+document.getElementById('result').textContent = res;
+}  
