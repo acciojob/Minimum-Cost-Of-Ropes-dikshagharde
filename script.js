d476ab9f924f0def69ef1a496d5e37028cec39d9
dikshagharde <117832798+dikshagharde@users.noreply.github.com>
2023-05-03 22:53:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-function calculateMinCost() {
-  const ropeLengthsStr = document.getElementById('rope-lengths').value;
-  const ropeLengthsArr = ropeLengthsStr.split(',').map(Number);
-  const n = ropeLengthsArr.length;
- 
-  let pq = [];
-  for (let i = 0; i < n; i++) {
-    pq.push(ropeLengthsArr[i]);
-  }
-  pq.sort(function (a, b) { return a - b; });
- 
-  let res = 0;
-  while (pq.length > 1) {
-    let first = pq.shift();
-    let second = pq.shift();
-    res += first + second;
-    pq.push(first + second);
-    pq.sort(function (a, b) { return a - b; });
-  }
- 
-  document.getElementById('result').textContent = res;
+//your code here
+function mincosttoformrope(a) {
+	a.preventDefault();//to stop vanishing data on refreshing page 
+var inputElement=document.querySelector('input').value;
+var arr=inputElement.split(',');// to convert into array
+	arr.sort(function(a,b) {return a-b});//a-b sort in increasing order b-a for decreeazing
+	var cost=0;
+	while(arr.length>1)
+		{
+			var res= Number(arr[0])+Number(arr[1]);
+			arr.shift();
+			arr.shift();//arr.splice(0,2);
+			arr.push(res);
+			cost+=res;
+			
+			arr.sort(function(a,b){return a-b});//data is being sort again and again so that i get the minimum element at front after push
+			
+		}
+	document.getElementById("result").textContent=cost;
+
+	
 }
  
